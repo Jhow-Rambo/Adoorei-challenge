da4904e31795a8bcada8aaadecb3d47b7e083d3b
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-04-15 14:55:24 -0300

    feat: add default colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   purge: ["./src/**/*.{vue,js}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#FFF",
+      black: "#000",
+      "primary-gray": "#292D32",
+      "secondary-gray": "#666666",
+      "primary-pink": "#F30168",
+    },
   },
   variants: {},
   plugins: [],
