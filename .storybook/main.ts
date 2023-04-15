755df700a1e7a2534f5ab70963260cb6a560f0aa
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-04-15 14:53:41 -0300

    refactor: change storybook configs, set tailwind

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,23 @@
 import type { StorybookConfig } from "@storybook/vue3-webpack5";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-docs",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        cssLoaderOptions: {
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/vue3-webpack5",
