3362efaae60b37c0bc1efbee32d82cad7a772d6d
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-04-15 18:36:14 -0300

    refactor: changed 'test' to 'it' in Input spec to keep the pattern

--- tests/unit/Input.spec.js
+++ tests/unit/Input.spec.js
@@ -2,7 +2,7 @@ import { mount } from "@vue/test-utils";
 import InputComponent from "../../src/components/Input.vue";
 
 describe("InputComponent", () => {
-  test("renders correctly", () => {
+  it("renders correctly", () => {
     const wrapper = mount(InputComponent, {
       propsData: {
         label: "Label",
@@ -13,7 +13,7 @@ describe("InputComponent", () => {
     expect(wrapper.element).toMatchSnapshot();
   });
 
-  test("emits input event when input is changed", () => {
+  it("emits input event when input is changed", () => {
     const wrapper = mount(InputComponent, {
       propsData: {
         label: "Label",
