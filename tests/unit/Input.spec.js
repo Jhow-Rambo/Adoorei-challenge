273ff64a7bd11f615d02d420aa27f0819db14eb4
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-04-16 01:37:13 -0300

    test: add downLabel test in Input.spec file

--- tests/unit/Input.spec.js
+++ tests/unit/Input.spec.js
@@ -25,4 +25,21 @@ describe("InputComponent", () => {
     input.setValue("New Value");
     expect(wrapper.emitted().input[0]).toEqual(["New Value"]);
   });
+
+  it("renders the down label when provided", () => {
+    const downLabel = {
+      message: "Lorem Ipsum",
+      isOnTheRight: true,
+    };
+    const wrapper = mount(InputComponent, {
+      propsData: {
+        label: "Label",
+        placeholder: "Placeholder",
+        downLabel: downLabel,
+      },
+    });
+
+    const downLabelEl = wrapper.find(".flex.justify-end label");
+    expect(downLabelEl.text()).toBe(downLabel.message);
+  });
 });
