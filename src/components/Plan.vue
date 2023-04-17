b3e95d5a9f96602b3f5720f75ea36095cbd3d1ff
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-04-17 02:29:30 -0300

    feat: create Plan component and Plan mocks

---
+++ src/components/Plan.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="relative max-w-[348px] border rounded border-ice-blue">
+    <span
+      v-if="isMostUsedPlan || isSelectedPlan"
+      class="absolute top-0 flex items-center justify-center px-6 py-2 transform -translate-x-1/2 -translate-y-1/2 rounded-xl left-1/2 whitespace-nowrap"
+      :class="{
+        'bg-black': isSelectedPlan,
+        'bg-primary-green': !isSelectedPlan,
+      }"
+    >
+      <p v-if="!isSelectedPlan" class="text-sm font-medium text-white">
+        MAIS USADO
+      </p>
+
+      <p v-else class="text-sm font-medium text-white">PLANO ESCOLHIDO</p>
+    </span>
+    <div
+      class="flex flex-col items-center w-full py-8 space-y-6 overflow-hidden text-center px-14"
+      :class="{ 'h-[700px]': isSelectedPlan }"
+    >
+      <p class="text-2xl font-bold text-secondary-gray">{{ planName }}</p>
+
+      <h2 v-if="price" class="text-4xl font-bold text-primary-pink">
+        <span class="text-2xl">R$</span>{{ price
+        }}<span class="text-2xl">/mês</span>
+      </h2>
+
+      <h2 v-else class="text-4xl font-bold text-center text-primary-pink">
+        Grátis
+      </h2>
+
+      <p class="text-sm font-normal text-secondary-gray">
+        {{ description }}
+      </p>
+
+      <div class="py-4 border border-y border-light-gray border-1 border-x-0">
+        <p class="text-lg font-normal text-secondary-gray">
+          {{ idealFor }}
+        </p>
+      </div>
+
+      <p
+        v-if="showPlan === false"
+        class="text-lg font-bold cursor-pointer text-secondary-gray md:hidden"
+        @click="toggleShowPlan"
+      >
+        Ver mais
+      </p>
+
+      <component :is="planDescription[planIndex]" v-if="showPlan" />
+
+      <p
+        v-if="showPlan === true"
+        class="text-lg font-bold cursor-pointer text-secondary-gray md:hidden"
+        @click="toggleShowPlan"
+      >
+        Ver menos
+      </p>
+
+      <div
+        v-if="isSelectedPlan"
+        class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-[348px] pt-8 px-14 bg-gradient-to-t from-transparent to-white"
+      >
+        <Button label="Trocar plano" class="w-full" />
+
+        <span class="block w-full h-10 bg-white"></span>
+      </div>
+
+      <div v-else>
+        <Button label="ESCOLHER ESSE PLANO" :is-primary="true" />
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Button from "./Button.vue";
+import Plan1 from "../mocks/Plan1.vue";
+import Plan2 from "../mocks/Plan2.vue";
+import Plan3 from "../mocks/Plan3.vue";
+import IPlanProps from "@/types/PlanProps";
+
+export default defineComponent({
+  name: "PlanComponent",
+  components: {
+    Button,
+  },
+  props: {
+    planName: {
+      type: String as PropType<IPlanProps["planName"]>,
+      required: true,
+    },
+    price: {
+      type: Number as PropType<IPlanProps["price"]>,
+      default: 0,
+    },
+    description: {
+      type: String as PropType<IPlanProps["description"]>,
+      required: true,
+    },
+    idealFor: {
+      type: String as PropType<IPlanProps["idealFor"]>,
+      required: true,
+    },
+    planIndex: {
+      type: Number as PropType<IPlanProps["planIndex"]>,
+      required: true,
+    },
+    isSelectedPlan: {
+      type: Boolean as PropType<IPlanProps["isSelectedPlan"]>,
+      default: false,
+    },
+    isMostUsedPlan: {
+      type: Boolean as PropType<IPlanProps["isSelectedPlan"]>,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      showPlan: true,
+      isMobile: false,
+      planDescription: {
+        0: Plan1,
+        1: Plan2,
+        2: Plan3,
+      } as { [k: number]: any },
+    };
+  },
+  methods: {
+    toggleShowPlan() {
+      this.showPlan = !this.showPlan;
+    },
+  },
+  mounted() {
+    this.isMobile = window.innerWidth < 768;
+    if (this.isMobile) this.showPlan = false;
+  },
+});
+</script>
---
+++ src/mocks/Plan2.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="flex flex-col space-y-4">
+    <div class="flex justify-start">
+      <p
+        class="mr-1 text-base font-bold leading-5 text-primary-gray text-start"
+      >
+        Seu site em servidores
+        <SmartLink label="no Brasil." :underline="true" href="/" />
+      </p>
+    </div>
+    <ul class="space-y-4 text-lg font-normal text-start text-secondary-gray">
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />Servidores
+        em nossos data center de São Paulo;
+      </li>
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />
+        ASP, ASP.NET 2.0/3.0/3.5/ e 4.0/4.5/4.5.1/4.5.2 (medium trust) ou PHP
+        5.3, 5.4, 5.5, 5.6 e 7.0;
+      </li>
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />10 usuário
+        de FTP para upload ou download;
+      </li>
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />30 contas
+        de e-mails profissionais;
+      </li>
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />10
+        subdomínio gratuito;
+      </li>
+    </ul>
+    <p class="mr-1 text-base font-bold leading-5 text-primary-gray text-start">
+      Suporte 24 horas, 7 dias por semana grátis;
+    </p>
+
+    <p class="mr-1 text-base font-bold leading-5 text-primary-gray text-start">
+      Aplicativos disponíveis;
+    </p>
+    <ul class="space-y-4 text-lg font-normal text-start text-secondary-gray">
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />Wordpress;
+      </li>
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />
+        Drupal;
+      </li>
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />entre
+        outros...
+      </li>
+    </ul>
+
+    <p class="mr-1 text-base font-bold leading-5 text-primary-gray text-start">
+      Migração Gratuita;
+    </p>
+    <ul class="space-y-4 text-lg font-normal text-start text-secondary-gray">
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />Migramos
+        todos seus sites para nossos servidores;
+      </li>
+    </ul>
+
+    <p class="mr-1 text-base font-bold leading-5 text-primary-gray text-start">
+      Você ainda tem
+    </p>
+    <ul class="space-y-4 text-lg font-normal text-start text-secondary-gray">
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />Webmail
+        RoundCube;
+      </li>
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />
+        AntiSpam;
+      </li>
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />
+        Painel de gerenciamento de DNS;
+      </li>
+
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />
+        Painel de controle web;
+      </li>
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />
+        Configurações de PHP personalizáveis;
+      </li>
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />
+        Certificado SSL Grátis;
+      </li>
+      <li class="flex align-text-top">
+        <CheckIcon fillColor="#292D32" size="15" class="mt-1 mr-2" />entre
+        Transferência ilimitada.
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import SmartLink from "../components/SmartLink.vue";
+import CheckIcon from "vue-material-design-icons/Check.vue";
+
+export default defineComponent({
+  name: "Plan2Component",
+  componente: {
+    SmartLink,
+    CheckIcon,
+  },
+});
+</script>
