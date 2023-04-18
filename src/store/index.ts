99d2230fad21d5dc7c28bab6e5be7669eab6ae31
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-04-18 04:45:48 -0300

    feat: add VuexPersistence library. Now data of storage is not lost when reload page

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,19 @@
 import { createStore } from "vuex";
-import { Plan, plan } from "./modules/plan";
+import { plan } from "./modules/plan";
+import IPlan from "@/types/Plan";
+import VuexPersistence from "vuex-persist";
 
 export interface State {
-  plan: Plan;
+  plan: IPlan;
 }
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
 
 export default createStore<State>({
   modules: {
     plan,
   },
+  plugins: [vuexLocal.plugin],
 });
